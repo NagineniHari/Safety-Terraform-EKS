ac7c343922739fb46eb16aa402b613a770786d83
naginenihari <naginenihari@gmail.com>
2025-12-16 18:50:53 +0530

    Green nodes creation

---
+++ 50-Green-EKS/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "remote-state-aws-terraform"
+    key          = "safety-dev-eks-remote-state-file"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+}

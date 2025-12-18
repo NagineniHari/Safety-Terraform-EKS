00d9f1294a42a8eb4917407fe6e8a284a63d9bfc
naginenihari <naginenihari@gmail.com>
2025-12-18 17:38:20 +0530

    Terraform EKS Cluster Version upgrade

--- Terraform-EKS_Cluster/provider.tf
+++ Terraform-EKS_Cluster/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket       = "remote-state-aws-terraform"
-    key          = "safety-dev-eks-blue-remote-state-file"
+    key          = "safety-dev-eks-terraform-blue-remote-state-file"
     region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
